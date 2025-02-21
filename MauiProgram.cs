086e10e8a6f68f37924d75880607c8e5a892d782
VonDefiant <fgomez2017ig@gmail.com>
2025-02-21 01:02:34 -0600

    sa

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -7,7 +7,7 @@ namespace DISMOGT_REPORTES;
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
-    {
+    { 
         var builder = MauiApp
             .CreateBuilder()
             .UseMauiApp<App>()
